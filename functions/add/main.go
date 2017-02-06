6f384f679344b1c0195629fc6cf8524d8505f8d5
DnOberon <johnw.darrington@gmail.com>
2017-02-05 17:44:21 -0700

    added styling, working on adding ids

--- functions/add/main.go
+++ functions/add/main.go
@@ -9,6 +9,8 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"io/ioutil"
+	"math/rand"
+	"time"
 )
 
 // Event data JSONing
@@ -81,6 +83,21 @@ func main() {
 			return ResponseMessage{Text: "Unable to add quote"}, err
 		}
 
+		if event.Text == "" {
+			s := rand.NewSource(time.Now().Unix())
+			r := rand.New(s) // initialize local pseudorandom generator
+			random := quotes.Quotes[r.Intn(len(quotes.Quotes))]
+
+			resp := ResponseMessage{Text: random.Text, ResponseType: "in_channel"}
+
+			if event.ChannelName == "it" {
+				resp.ResponseType = "ephemeral"
+			}
+
+			return resp, nil
+
+		}
+
 		// add and upload new quote
 		quotes.Quotes = append(quotes.Quotes, Quote{Text: event.Text, Author: event.UserName})
 
@@ -94,6 +111,7 @@ func main() {
 			Key:         aws.String("quotes.json"),       // Required
 			Body:        bytes.NewReader(newQuotes),
 			ContentType: aws.String("application/json"),
+			ACL:         aws.String("public-read"),
 		}
 
 		_, err = svc.PutObject(uploadParams)
@@ -101,6 +119,6 @@ func main() {
 			return ResponseMessage{Text: "Unable to add quote"}, err
 		}
 
-		return ResponseMessage{Text: "Quote added successfully!", ResponseType: "ephemeral"}, err
+		return ResponseMessage{Text: "Quote added successfully! Visit: http://shitjasonsays.com", ResponseType: "ephemeral"}, err
 	})
 }
--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+
 import './App.css';
 import 'whatwg-fetch';
+import 'bulma';
+import gopher from './jasongopher.png';
 
 class App extends Component {
 
@@ -12,44 +15,96 @@ class App extends Component {
 
         // fetch and set the quotes array to state
         fetch('/quotes.json')
-            .then((response) => {
-                return response.json()
-            }).then((json) => {
-                let quotes = json.quotes
-                let randQuote = quotes[Math.floor(Math.random() * quotes.length)];
+                               .then((response) => {
+                                   return response.json()
+                               }).then((json) => {
+                                   let quotes = json.quotes
+                                   let randQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
-                this.setState({
-                    "selectedQuote": randQuote.text,
-                    "quotes": quotes, 
-                })
+                                   this.setState({
+                                       "selectedQuote": randQuote.text,
+                                       "quotes": quotes,
+                                       "author": randQuote.author
+                                   })
 
 
-            }).catch((ex) => {
-                console.log('parsing failed', ex)
-                this.setState({"hasError": true, "errorMessage":ex, "selectedQuote": "I sat close to the microwave as a kid. Sometimes I got headaches."})
-            })
+                               }).catch((ex) => {
+                                   console.log('parsing failed', ex)
+                                   this.setState({"hasError": true,
+                                                  "errorMessage":ex,
+                                                  "selectedQuote": "I sat close to the microwave as a kid. Sometimes I got headaches.",
+                                                  "author": "johnthewayne"
+                                   })
+                               })
     }
 
     changeQuote() {
-        let newQuote = this.state.quotes[Math.floor(Math.random() * this.state.quotes.length)].text
+        let newQuote = this.state.quotes[Math.floor(Math.random() * this.state.quotes.length)]
 
-        while(newQuote === this.state.selectedQuote) {
-            newQuote = this.state.quotes[Math.floor(Math.random() * this.state.quotes.length)].text
+        while(newQuote.text === this.state.selectedQuote) {
+            newQuote = this.state.quotes[Math.floor(Math.random() * this.state.quotes.length)]
         }
 
         this.setState({
-            "selectedQuote" : newQuote
+            "selectedQuote" : newQuote.text,
+            "author": newQuote.author
         })
     }
 
-     render() {
+    render() {
         return (
-            <div className="App">
-                <div className="App-header">
-                    <h2>{this.state.selectedQuote}</h2>
-                    <div><button className="button" type="button" onClick={this.changeQuote}>New</button></div>
-                </div>
-            </div>
+
+            <section className="hero is-primary is-fullheight">
+                                <div className="">
+                    <div className="container">
+                        <div className="columns hero-body">
+                            <div className="column">
+                                <h1 className="title">
+                                    {this.state.selectedQuote}
+                                </h1>
+                                <h2 className="subtitle">
+                                    added by @{this.state.author}
+                                </h2>
+                                <div><a className="button  is-primary is-inverted is-outlined"  onClick={this.changeQuote}>New</a></div>
+                            </div>
+                            <div className="column">
+                                <figure className="image is-square">
+                                    <img src={gopher}  alt="gopher"/>
+                                </figure>
+                            </div>
+                        </div>
+                    </div>
+                                </div>
+
+
+                                <div className="hero-foot">
+                                    <header className="nav">
+                                        <div className="container">
+                                            <div className="nav-left">
+                                                <a className="nav-item">
+                                                    Shit Jason Says
+                                                </a>
+                                            </div>
+                                            <span className="nav-toggle">
+                                                <span></span>
+                                                <span></span>
+                                                <span></span>
+                                            </span>
+                                            <div className="nav-right nav-menu">
+                                                <span className="nav-item">
+                                                    <a className="button is-primary is-outlined is-inverted" href="https://github.com/DnOberon/sjs" target="_blank">
+                                                        <span className="icon">
+                                                            <i className="fa fa-github"></i>
+                                                        </span>
+                                                        <span>Now with more open source!</span>
+                                                    </a>
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </header>
+                                </div>
+
+            </section>
         );
     }
 }
