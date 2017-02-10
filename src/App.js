ae1b9b8626b118b00dc0fc50e170c7dfb42d6f16
DnOberon <johnw.darrington@gmail.com>
2017-02-09 22:06:54 -0700

    removed css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { hashHistory } from 'react-router';
 
-import './App.css';
 import 'whatwg-fetch';
 import 'bulma';
 import gopher from './jasongopher.png';
